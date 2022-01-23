46897f7c049d2d0ac15ba003457339b70ecf8535
tom <tommy.dumont@uqtr.ca>
2022-01-22 21:29:14 -0500

    basic understanding test

--- app/src/main/java/com/example/testdevmobile/MainActivity.java
+++ app/src/main/java/com/example/testdevmobile/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.testdevmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    int compteur;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onOpen(View view) {
+        Intent profileItent = new Intent(this, Profile.class);
+        profileItent.putExtra("name", "bork");
+        profileItent.putExtra("car", "civique");
+        startActivity(profileItent);
+    }
+
+    public void openCam(View view) {
+        Intent cam = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivity(cam);
+    }
+
+    public void additions(){
+
     }
 }
---
+++ app/src/main/java/com/example/testdevmobile/Profile.java
@@ -0,0 +1,18 @@
+package com.example.testdevmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        String nom = getIntent().getExtras().getString("name");
+    }
+
+
+}
