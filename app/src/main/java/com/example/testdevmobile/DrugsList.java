a10f8825db31b3f89c238b2277d970f994f9a2c0
tom <tommy.dumont@uqtr.ca>
2022-02-12 12:38:50 -0500

    simple fragment working and databinding added

--- app/src/main/java/com/example/testdevmobile/DrugsList.java
+++ app/src/main/java/com/example/testdevmobile/DrugsList.java
@@ -5,16 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.Inflater;
 
-public class DrugsList extends AppCompatActivity {
+public class DrugsList extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +31,31 @@ public class DrugsList extends AppCompatActivity {
         drugList.add("pillule");
         drugList.add("grosse pillule");
         drugList.add("minecraft pill");
-//        TextView texteprincipale = findViewById(R.id.texte1);
-//        texteprincipale.setText("akki");
+
+
         ArrayAdapter drugListAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1 , drugList);
         ListView drugListView = (ListView) findViewById(R.id.drug_List);
         createRandom(drugList);
         drugListView.setAdapter(drugListAdapter);
+
+        drugListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
     }
+
+    public void onCreateContextMenu(ContextMenu menu, View view,ContextMenu.ContextMenuInfo menuInfo) {
+        ListView list = (ListView)view;
+
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        int position = info.position;
+
+
+    }
+
     public void createRandom(ArrayList<String> druglist){
         for(int i =0; i<30; i++){
             druglist.add("allo"+i);
