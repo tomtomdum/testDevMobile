28ff3f3989acd4f38db30df45e80da8f40d07e45
Tommy Dumont <t.dumont@qosma.ca>
2022-02-17 18:57:38 -0500

    pas grand choses

---
+++ app/src/main/java/com/example/testdevmobile/MonApp.java
@@ -0,0 +1,11 @@
+package com.example.testdevmobile;
+
+import androidx.multidex.MultiDexApplication;
+
+public class MonApp extends MultiDexApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
